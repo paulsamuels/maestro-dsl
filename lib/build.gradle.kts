49a020e92e1b20a48d884849b051e94c0a3d2489
Paul Samuels <paul.samuels@autotrader.co.uk>
2023-12-07 23:25:03 +0000

    Attempt publishing

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,10 +2,16 @@ plugins {
     kotlin("jvm") version "1.9.10"
     `java-library`
     `maven-publish`
+    `signing`
 }
 
 group = "com.paul-samuels"
-version = "0.1.0"
+version = "0.0.1"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
 
 repositories {
     mavenCentral()
@@ -22,11 +28,58 @@ dependencies {
 
 publishing {
     publications {
-        create<MavenPublication>("maestro.dsl") {
+        create<MavenPublication>("maestro-dsl") {
             artifactId = "maestro-dsl"
             from(components["java"])
+
+            pom {
+                name = "Maestro Dsl"
+                description = "A Kotlin Dsl for building Maestro scripts"
+                url = "https://github.com/paulsamuels/maestro-dsl"
+
+                developers {
+                    developer {
+                        id = "paulsamuels"
+                        name = "Paul Samuels"
+                        email = "paulio1987@gmail.com"
+                    }
+                }
+
+                scm {
+                    url = "https://github.com/paulsamuels/maestro-dsl"
+                }
+
+                licenses {
+                    license {
+                        name = "MIT"
+                    }
+                }
+            }
         }
     }
+
+    repositories {
+        maven {
+            url = uri(
+                if (version.toString().endsWith("SNAPSHOT")) {
+                    "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                } else {
+                    "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                },
+            )
+            credentials {
+                val ossrhUsername: String by properties
+                val ossrhPassword: String by properties
+
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
 
 kotlin {
